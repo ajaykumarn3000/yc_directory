647a74c411878ca46372b76ef8176bbf2bf32134
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2024-11-01 22:20:30 +0530

    All Startups & Startup Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,16 +7,37 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams)?.query;
+
+  const posts = [
+    {
+      _createdAt: new Date(), views: 55, author: {
+        _id: 1,
+        name: "Ajaykumar Nadar"
+      },
+      _id: 1,
+      description: "This is a description",
+      image: "https://cdn.ebaumsworld.com/mediaFiles/picture/2452130/85386506.jpg",
+      category: "Robots",
+      title: "We Robots"
+    },
+  ]
+
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">
-          Pitch your Startup, Connect with Entreprenuer
+          Pitch your Startup, Connect with Entrepreneur
         </h1>
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competition
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold text-black">{query ? `Search results for ${query}` : "All Startups"}</p>
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? posts.map((post: StartupCardType) => <StartupCard key={post?._id} post={post} />) : null}
+        </ul>
       </section>
     </>
   );
