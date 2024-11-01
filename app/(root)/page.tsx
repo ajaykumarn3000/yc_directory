0df233e594ebeb705f9f0b4cd2b17442245a53d2
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2024-11-01 20:00:31 +0530

    Search Form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,21 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams)?.query;
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">
           Pitch your Startup, Connect with Entreprenuer
         </h1>
-        <p className="sub_heading !max-w-3xl">
+        <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competition
         </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
