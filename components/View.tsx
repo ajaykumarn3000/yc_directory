5e50435c396f75b526a556acd49dd64b862047c2
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2024-11-02 23:46:12 +0530

    View Count [Sanity Write Client]

--- components/View.tsx
+++ components/View.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Ping from './Ping'
 import { client } from '@/sanity/lib/client'
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import { writeClient } from '@/sanity/lib/write-client'
+import { unstable_after as after } from 'next/server'
 
 const View = async ({ id }: { id: string }) => {
 
@@ -10,7 +12,15 @@ const View = async ({ id }: { id: string }) => {
       .withConfig({ useCdn: false })
       .fetch(STARTUP_VIEWS_QUERY, { id });
 
-  // TODO: Update the views count in the database
+  after(
+    async () => (
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+    )
+  )
+
 
   return (
     <div className='view-container'>
